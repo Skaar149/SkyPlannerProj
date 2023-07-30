b627739c56b42f6aac76eb944cb3f04029e3dd74
Jesus <jeos149@gmail.com>
2023-07-30 16:22:57 -0400

    Adding logic for  the colors of the roles

--- force-app/main/default/lwc/contactRoleListComponent/contactRoleListComponent.js
+++ force-app/main/default/lwc/contactRoleListComponent/contactRoleListComponent.js
@@ -20,23 +20,7 @@ export default class ContactRoleListComponent extends LightningElement {
       .then((result) => {
         console.log("entro getcontactRoleListString ");
         console.log("getcontactRoleListString " + JSON.stringify(result));
-        this.contactRoleListJson =  JSON.parse(result[0]);
-        console.log(
-          "this.contactRoleListJson " +
-          JSON.stringify(this.contactRoleListJson)
-        );
-        console.log(
-          "this.contactRoleListJson [0].contactName " +
-            JSON.stringify(
-              this.contactRoleListJson.contactName
-            )
-        );
-        console.log('antes del foreach ' );
-        this.contactRoleList = [];
-        result.forEach(contactRole =>{
-          this.contactRoleList.push(JSON.parse(contactRole))
-          console.log('foreach element ' + JSON.stringify(JSON.parse(contactRole)))
-      });
+        this.contactRoleList = result;
       })
       .catch((error) => {
         this.errorMessage = error;
